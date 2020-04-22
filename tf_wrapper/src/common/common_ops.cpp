2c37724018357cf1cbd97aeea0c3c22266c84d7c
ucLh <luchwork@mail.ru>
2020-04-22 13:55:32 +0300

    Modified class extraction for metrics

--- tf_wrapper/src/common/common_ops.cpp
+++ tf_wrapper/src/common/common_ops.cpp
@@ -40,3 +40,47 @@ std::string common_ops::extract_class(const std::string &filepath) {
 
   return token;
 }
+
+std::vector<std::string> common_ops::split(const std::string &text,
+                                           const std::string &delim) {
+  std::vector<std::string> results = {};
+  for (auto p = cbegin(text); p != cend(text);) {
+    const auto n = search(p, cend(text), cbegin(delim), cend(delim));
+    results.emplace_back(p, n);
+    p = n;
+    if (cend(text) != n) // we found delim, skip over it.
+      p += delim.length();
+  }
+  return results;
+}
+
+std::string common_ops::check_end_slash(const std::string &path) {
+  std::string result = path;
+  if (result[result.size() - 1] == '/')
+    result.pop_back();
+  return result;
+}
+
+std::string common_ops::extract_class(const std::string &path_to_img,
+                                       const std::string &series_path,
+                                       const std::string &queries_path) {
+  size_t class_pos = 0;
+  std::string series_path_checked = check_end_slash(series_path);
+  std::string queries_path_checked = check_end_slash(queries_path);
+  auto series_dir = split(series_path_checked, "/").back();
+  auto queries_dir = split(queries_path_checked, "/").back();
+
+  size_t in_queries_pos = path_to_img.find(queries_dir);
+  size_t in_imgs_pos = path_to_img.find(series_dir);
+
+  if (in_queries_pos == std::string::npos)
+    class_pos = in_imgs_pos + series_dir.size() + 1;
+  else
+    class_pos = in_queries_pos + queries_dir.size() + 1;
+
+  std::string relative_img_path =
+      path_to_img.substr(class_pos, std::string::npos);
+  std::string classname = split(relative_img_path, "/")[0];
+  classname = split(classname, "__")[0];
+  return classname;
+}
